fe3cd1d65e3f9b96d1c14c2a6754a922fcb14a49
Wei-Fan <zwf_jaccount@sjtu.edu.cn>
2018-01-11 14:58:07 +0800

    Update z_main_alpha.m

--- z_main_alpha.m
+++ z_main_alpha.m
@@ -1,135 +1,98 @@
-function [] = z_main_alpha
 %initialize serial
 delete(instrfindall);
 blu = serial('com3');
 set(blu,'BaudRate',9600);
 fopen(blu);
 
+distance = 5;
+direction = 0;
+
 %initialize camera
 vid = videoinput('winvideo',1);  
 %preview(vid);  
 
-%initialization
-count = 1; X = zeros(1,5); Y = zeros(1,5);
-ROI_flag = 0;
-width = 0; length = 0;
-
 % Configure the object for manual trigger mode.
 triggerconfig(vid, 'manual');
 
-% Now that the device is configured for manual triggering, call START.
-% This will cause the device to send data back to MATLAB, but will not log
-% frames to memory at this point.
 start(vid)
-src0 = getsnapshot(vid);
+tmp0 = getsnapshot(vid);
+src0 = ycbcr2rgb(tmp0);
 [img_x, img_y, ~] = size(src0);
-figure('name','z_trajectory_measement');
-while (1)
-    src = getsnapshot(vid);
-    %find the ROI
-    if ROI_flag == 0
-        [x1,y1,w,l] = z_dynamic_ROI(src0,src);
-        src0 = src;
-        if x1 == -1
-            fprintf('No find ROI~~\n')
-            continue;
-        else
-            ROI_flag = 1;
-            width = w; length = l;
-        end
+count = 1;
+X = zeros(1,4);
+Y = zeros(1,4);
+
+while ~strcmpi(get(gcf,'CurrentCharacter'),'e')
+    tmp1 = getsnapshot(vid);
+    src1 = ycbcr2rgb(tmp1);
+    figure(1)
+    imshow(src1);
+    R0=src0(:,:,1);
+    R1=src1(:,:,1);
+    
+    threshold = zeros(img_x,img_y,'single');
+    for i=1:img_x
+       for j=1:img_y
+           if R1(i,j)-R0(i,j)>12 || R0(i,j)-R1(i,j)>12
+               threshold(i,j) = 255;
+           else
+               threshold(i,j) = 0;
+           end
+       end
+    end
+    threshold_small = threshold - bwareaopen(threshold,10);
+    threshold_big = bwareaopen(threshold,300);
+    threshold = threshold - threshold_big - threshold_small;
+    figure(2)
+    imshow(threshold),title('threshold');
+    
+    stats = regionprops(threshold, 'basic');
+    s = size(stats);
+    s = s(1);
+    if(s==0)
+        fprintf('no trash detected~~\n')
+        src0 = src1;
+        continue;
+    elseif(s>1)
+        fprintf('noise too loud~~\n')
+        src0 = src1;
+        continue;
     end
 
-    %detect the position of the ball
-    [x,y] = z_ball_detect(src,x1,y1,w,l);
-    if x == -1
-        src0 = src;
-        ROI_flag = 0;
-        %count = 1;
-        continue;
-    end
+    x = stats(s).Centroid(1);
+    y = stats(s).Centroid(2);
     
-    %get the trajectory
-    if count == 1
-        X(1) = x;
-        Y(1) = y;
-        count = count + 1;
-        continue;
-    elseif count < 6
+    if count <= 4
         X(count) = x;
         Y(count) = y;
-        x1 = x1 + 1.15*(X(count) - X(count-1));
-        y1 = y1 + 1.25*(Y(count) - Y(count-1));
-        if x1 < 1
-            x1 = 1;
-        end
-        if y1 < 1
-            y1 = 1;
-        end
-        if x1 + width > img_y
-            w = img_y - x1;
-        else
-            w = width;
-        end
-        if y1 + length > img_x
-            l = img_x - y1;
-        else
-            l = length;
-        end
         count = count + 1;
-        continue;
-    elseif count == 6
-        for i = 1:4
-            X(i) = X(i+1);
-            Y(i) = Y(i+1); 
+    elseif count == 5
+        a = z_curveFit(X, -Y, 2)
+        
+        if a(1)<0
+            %test
+            %t = 0:1280;
+            %p = a(1) + a(2)*t + a(3)*t.^2;
+            %figure(3);plot(t,p),title('track');
+            %[distance,direction] = z_transfer(a(1),a(2),a(3));
+            if distance == 0
+                fprintf(blu,'%s','A');
+            else
+                fprintf(blu,'%s','E');
+            end
+            pause(distance)
+            fprintf(blu,'%s',Z);
         end
-        X(5) = x;
-        Y(5) = y;
+        
+        count = 1;
+        src0 = src1;
+        continue;
     end
-    
-    x1 = x1 + X(5) - X(4);
-    y1 = y1 + Y(5) - Y(4);
-    if x1 < 1
-        x1 = 1;
-    end
-    if y1 < 1
-        y1 = 1;
-    end
-    if x1 + width > img_y
-        w = img_y - x1;
-    else
-        w = width;
-    end
-    if y1 + length > img_x
-        l = img_x - y1;
-    else
-        l = length;
-    end
-    
-    a = z_curveFit(X, -Y, 2);
-    %testing
-    imshow(src)
-    hold on
-    rectangle('Position',[x1,y1,w,l],'edgecolor','r');
-    hold off
-    
-    [distance,direction] = z_transfer(a(1),a(2),a(3));
-    fprintf(blu,'%d',distance);
-    fprintf(blu,'%d',direction);
-    fprintf(blu,'%d',Z);
-    for i = 1:5
-        fprintf(blu,'%d',direction);
-    end
-    fprintf(blu,'%d',Z);
-    
-    if strcmpi(get(gcf,'CurrentCharacter'),'e')
-        break;
-    end
+    src0 = src1;
 end
 stop(vid)
 delete(vid)
-close
+close all
 
 fclose(blu);
 delete(blu);
-end
-
